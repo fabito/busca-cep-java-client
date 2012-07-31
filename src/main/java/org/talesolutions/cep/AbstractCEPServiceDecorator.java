f30bd774e9e758ca4fac310ad93dcea05f4dc4fb
fabito <fabio.uechi@gmail.com>
2012-07-30 22:22:39 -0300

    documentando o decorator base

--- src/main/java/org/talesolutions/cep/AbstractCEPServiceDecorator.java
+++ src/main/java/org/talesolutions/cep/AbstractCEPServiceDecorator.java
@@ -1,5 +1,10 @@
 package org.talesolutions.cep;
 
+/**
+ * Decorator base para criação de decorators de {@link CEPService}
+ * @author Fábio Franco Uechi
+ *
+ */
 public abstract class AbstractCEPServiceDecorator implements CEPService {
 
 	protected CEPService decorated;
